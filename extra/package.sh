9c2ddba4da0289374114278d14b8d0903710c5aa
egornovivan <60801856+egornovivan@users.noreply.github.com>
2021-08-12 00:59:12 +0500

    Fix casdy dir

--- extra/package.sh
+++ extra/package.sh
@@ -15,19 +15,19 @@ cd "$src"
 for actor in $(ls); do
   cd "$actor"
   rm -rf "$dst/$actor" "$dst/${actor}_hq"
-  mkdir -p "$dst/$actor/sound/speech" "$dst/${actor}_hq/sound/speech"
+  mkdir -p "$dst/$actor/sound/speech/casdy" "$dst/${actor}_hq/sound/speech/casdy"
   for wav in $(ls); do
     # I assume ffmpeg is better at resampling than snd2acm
-    ffmpeg -i "$wav" -ac 2 -ar 44100 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}_hq/sound/speech/$wav"
-    ffmpeg -i "$wav" -ac 2 -ar 22050 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}/sound/speech/$wav"
+    ffmpeg -i "$wav" -ac 2 -ar 44100 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}_hq/sound/speech/casdy/$wav"
+    ffmpeg -i "$wav" -ac 2 -ar 22050 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}/sound/speech/casdy/$wav"
   done
-  cd "$dst/${actor}/sound/speech"
+  cd "$dst/${actor}/sound/speech/casdy"
   cp "$bin_dir/snd2acm.exe" .
   for wav in $(ls *.wav); do
     $wav2lip -i "$wav"
   done
   rm -f *.wav
-  cd "$dst/${actor}_hq/sound/speech"
+  cd "$dst/${actor}_hq/sound/speech/casdy"
   rm -f snd2acm.exe
   for wav in $(ls *.wav); do
     $wav2lip -i "$wav" -noACM
