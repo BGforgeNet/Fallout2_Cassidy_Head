f0d436831233f69149d820993e7131a7d5b68e3f
egornovivan <60801856+egornovivan@users.noreply.github.com>
2021-08-12 03:11:27 +0500

    test

--- extra/package.sh
+++ extra/package.sh
@@ -35,7 +35,7 @@ for actor in $(ls); do
   rm -f *.wav
   cd "$src/$actor"
   for wav in $(ls); do
-    ffmpeg -i "$wav" -ac 1 -ar 88200 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}_hq/sound/speech/casdy/$wav"
+    ffmpeg -i "$wav" -ac 2 -ar 88200 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}_hq/sound/speech/casdy/$wav"
     ffmpeg -i "$wav" -ac 1 -ar 44100 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1 "$dst/${actor}_lq/sound/speech/casdy/$wav"
   done
   cd "$dst/${actor}_hq/sound/speech/casdy"
