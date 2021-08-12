09e7c2350b4585de284b70328c2ecc00542acd00
Magus <magus@bgforge.net>
2021-08-13 03:02:23 +0700

    download ipsdoc from its repo

--- extra/package.sh
+++ extra/package.sh
@@ -11,6 +11,13 @@ head_dir="$(realpath head)"
 src="$(realpath sound_src)"
 dst="$(realpath sound_out)"
 ffmpeg_args="-c:a pcm_s16le -fflags +bitexact -flags:v +bitexact -flags:a +bitexact -map_metadata -1"
+ipsdoc_repo="BGforgeNet/ipsdoc"
+ipsdoc_release_url="https://api.github.com/repos/$ipsdoc_repo/releases/latest"
+ipsdoc="$bin_dir/ipsdoc"
+
+# get latest ipsdoc
+curl -s -O "$ipsdoc" "$(curl -s $ipsdoc_release_url | grep browser_download_url | grep -v '\.exe' | awk -F '"' '{print $4}')"
+chmod +x $ipsdoc
 
 cd "$src"
 for actor in $(ls); do
@@ -25,13 +32,13 @@ for actor in $(ls); do
   cd "$dst/${actor}_hq/sound/speech/casdy"
   for wav in $(ls *.wav); do
     acm="$(echo $wav | sed 's|\.wav|.acm|')"
-    ipsdoc "$wav" "$acm"
+    $ipsdoc "$wav" "$acm"
   done
   rm -f *.wav
   cd "$dst/${actor}_lq/sound/speech/casdy"
   for wav in $(ls *.wav); do
     acm="$(echo $wav | sed 's|\.wav|.acm|')"
-    ipsdoc "$wav" "$acm"
+    $ipsdoc "$wav" "$acm"
   done
   rm -f *.wav
   cd "$src/$actor"
